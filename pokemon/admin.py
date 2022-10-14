8086d494178dcb8c27b694dfe0d89d8fc901217e
phalkandari <mohammad.kandary@gmail.com>
2022-10-14 10:32:37 +0300

    Finished 1st part and passed all pytest tests

--- pokemon/admin.py
+++ pokemon/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Pokemon
 # Register your models here.
+
+
+@admin.register(Pokemon)  # This is a decorator
+class PokemonAdmin(admin.ModelAdmin):
+    list_diplay = ("name", )
--- pokemon/models.py
+++ pokemon/models.py
@@ -1,5 +1,5 @@
-from email.policy import default
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
 
@@ -13,13 +13,19 @@ class Pokemon (models.Model):
         FAIRY = 'FA'
 
     name = models.CharField(max_length=30)
-    type = models.CharField(max_length=30, choices=PokemonType.choices)
-    hp = models.PositiveBigIntegerField(default=True)
+    type = models.CharField(max_length=2, choices=PokemonType.choices)
+
+    hp = models.PositiveIntegerField(
+        validators=[MinValueValidator(50, "can't be a weakling!"), MaxValueValidator(350, "can't be a God!")])
     active = models.BooleanField(default=True)
 
-    name_fr: models.CharField(max_length=30, default="")
-    name_ar: models.CharField(max_length=30, default="")
-    name_jp: models.CharField(max_length=30, default="")
+    # For Internationalization
+    name_fr = models.CharField(max_length=30, default="", blank=True)
+    name_ar = models.CharField(max_length=30, default="", blank=True)
+    name_jp = models.CharField(max_length=30, default="", blank=True)
 
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
